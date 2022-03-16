3a6d68730aacc5b01f011091f7cad0c9e0895c6a
Nathan764 <nathanoli330@gmail.com>
2022-03-16 20:18:50 -0300

    imersao-dev-04

---
+++ funcoes.js
@@ -0,0 +1,8 @@
+var listaFilmes = [
+"https://th.bing.com/th/id/OIP.ot7eYL-0dTr8gI6T3vsKUAHaKf?pid=ImgDet&rs=1",
+"https://http2.mlstatic.com/pantera-negra-filme-2018-digital-dublado-e-legendado-D_NQ_NP_830796-MLB26904224818_022018-F.jpg",
+"https://th.bing.com/th/id/R.9799db00b16c49a354510bd97d5b58bb?rik=n54x7m1q2V1Ryg&pid=ImgRaw&r=0"];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+}
